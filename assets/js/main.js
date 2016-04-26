819aac1366594d6e9a9540063f8101003f5a4aa5
Tahseen Chowdhury <sendmemail@tahseenchowdhury.com>
2016-04-26 12:47:09 -0400

    website up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,118 @@
 /*
-	Visualize by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+	Polymorph by Pixelarity
+	pixelarity.com @pixelarity
+	License: pixelarity.com/license
 */
 
-$(function() {
+(function($) {
 
-	// Vars.
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)',
+		xxsmall: '(max-width: 360px)'
+	});
+
+	$(function() {
+
 		var	$window = $(window),
 			$body = $('body'),
-			$wrapper = $('#wrapper');
+			$header = $('#header');
 
-	// Breakpoints.
-		skel.breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
-		});
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
 
-	// Disable animations/transitions until everything's loaded.
-		$body.addClass('is-loading');
-
-		$window.on('load', function() {
-			$body.removeClass('is-loading');
-		});
-
-	// Poptrox.
-		$window.on('load', function() {
-
-			$('.thumbnails').poptrox({
-				onPopupClose: function() { $body.removeClass('is-covered'); },
-				onPopupOpen: function() { $body.addClass('is-covered'); },
-				baseZIndex: 10001,
-				useBodyOverflow: false,
-				usePopupEasyClose: true,
-				overlayColor: '#000000',
-				overlayOpacity: 0.75,
-				popupLoaderText: '',
-				fadeSpeed: 500,
-				usePopupDefaultStyling: false,
-				windowMargin: (skel.breakpoint('small').active ? 5 : 50)
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
 			});
 
-		});
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
 
-});
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				alignment: 'center'
+			});
+
+		// Banners.
+			$('#banner > article')
+				.css('cursor', 'pointer')
+				.on('click', 'a', function(event) {
+					event.stopPropagation();
+				})
+				.on('click', function(event) {
+
+					event.preventDefault();
+					event.stopPropagation();
+
+					var $a = $(this).find('a');
+
+					if ($a.length > 0)
+						window.location.href = $a.attr('href');
+
+				})
+				.each(function() {
+
+					var $this = $(this),
+						$img = $this.children('img'),
+						x;
+
+					// Assign image.
+						$this.css('background-image', 'url(' + $img.attr('src') + ')');
+
+					// Set background position.
+						if (x = $img.data('position'))
+							$this.css('background-position', x);
+
+					// Hide <img>.
+						$img.hide();
+
+				});
+
+		// Off-Canvas Navigation.
+
+			// Navigation Panel Toggle.
+				$('<a href="#navPanel" class="navPanelToggle">Menu</a>')
+					.appendTo($header);
+
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						'<nav>' +
+							$('#nav').navList() +
+						'</nav>' +
+						'<a href="#navPanel" class="close"></a>' +
+					'</div>'
+				)
+					.appendTo($body)
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'right',
+						target: $body,
+						visibleClass: 'is-menu-visible'
+					});
+
+			// Fix: Remove transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#navPanel')
+						.css('transition', 'none');
+
+	});
+
+})(jQuery);
